6f1d615e04ddc50861f1d6bbcd03f5937b5550e5
Harshil Patel <26harshilpatel11@gmail.com>
2024-06-30 15:03:00 -0400

    add algorithm 'Fast and slow pointer' ;

--- Easy/141. Linked List Cycle/app.py
+++ Easy/141. Linked List Cycle/app.py
@@ -19,3 +19,32 @@ class Solution(object):
                 nodeHash[start]= True
                 start=start.next
         return False
+    
+# Using Fast and slow pointers for sloving this problem with constant time complexity
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head==None or head.next==None or head.next.next==None:
+            return False
+        p1=head.next
+        p2=p1.next
+
+        while p2!=None:
+            if p1==p2:
+                return True
+            else:
+                p1=p1.next
+                if p2.next == None or p2.next.next==None:
+                    return False
+                else:
+                    p2=p2.next.next
