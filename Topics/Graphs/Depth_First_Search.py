4d9ae23c305cae6705ff7a65d765a09742b1d1ea
Harshil Patel <26harshilpatel11@gmail.com>
2024-07-02 20:20:04 -0400

    added 797. All Paths From Source to Target

---
+++ Topics/Graphs/Depth_First_Search.py
@@ -0,0 +1,13 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start, end=" ")
+
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+visited=set()
+dfs([[2,1],[3],[3],[]], 0, visited)
+# print(visited)
