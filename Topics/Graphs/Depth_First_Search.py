0ee6fb87a615ad7f5591ed3ef211786c81880076
Harshil Patel <100322816+hpatel292-seneca@users.noreply.github.com>
2024-07-06 17:18:21 +0000

    added Graph Depth first search algorithm with loops

--- Topics/Graphs/Depth_First_Search.py
+++ Topics/Graphs/Depth_First_Search.py
@@ -1,3 +1,4 @@
+# Recursive solution we are not using any Stack for storing elements as we are making recursive calls.
 def dfs(graph, start, visited=None):
     if visited is None:
         visited = set()
@@ -11,3 +12,22 @@ def dfs(graph, start, visited=None):
 visited=set()
 dfs([[2,1],[3],[3],[]], 0, visited)
 # print(visited)
+
+
+# Iterative way for DFS.
+def dfs_iterative(graph):
+    stack=[]
+    visited=set()
+    stack.append(0)
+    arr=[]
+    while stack:
+        curr=stack.pop()
+        visited.add(curr)
+        arr.append(curr)
+        for i in graph[curr]:
+            if i not in visited:
+                stack.append(i)
+        
+    return arr
+
+print(dfs_iterative([[2,1],[3],[3],[]]))
